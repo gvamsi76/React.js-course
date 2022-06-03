3086ef7a4ec9f25731cde653d075c2d6be0283a9
Vamshi <gvamshi906@gmail.com>
2022-06-03 18:33:13 +0530

    useState-hook

---
+++ src/components/Class.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Class extends Component {
+  render() {
+    return (
+      <div>Class</div>
+    )
+  }
+}
+
+export default Class
---
+++ src/components/Task.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react'
+
+function Task() {
+ const [data, setData] = useState(null);
+const [print,setPrint] = useState(false)
+function getData(val){
+   // console.warn(val.target.value)
+    setData(val.target.value)
+    setPrint(false)
+}
+
+  return (
+    <div>
+        {
+            print ? 
+            <h2>{data}</h2> :null
+        }
+        <input type="text"  placeholder="enter your name" onChange={getData} />
+        <button onClick={()=>setPrint(true)} >Submit</button>
+    </div>
+  )
+}
+
+export default Task
